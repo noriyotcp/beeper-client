e6f75499390d13f6b3cfb9ba932ad1ca606a5a2d
noriyotcp <noriyo.akita@gmail.com>
2017-04-22 05:34:41 +0900

    Add components for auth to auth directory

---
+++ src/components/auth/Auth.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: "auth"
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/auth/Login.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style lang="css">
+</style>
