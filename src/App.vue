30970be68df81b4ce3743ea5cb542c76c47d4878
noriyotcp <noriyo.akita@gmail.com>
2017-04-22 05:10:29 +0900

    [App.vue] Make the root page blank

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
 </template>
 
 <script>
@@ -17,12 +13,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
