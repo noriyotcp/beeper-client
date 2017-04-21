8f6e49c13201cf67f1fb45c05998943debf337bd
noriyotcp <noriyo.akita@gmail.com>
2017-04-22 05:07:31 +0900

    Import VueRouter

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Router from './routes';
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router: Router,
   template: '<App/>',
   components: { App }
 })
