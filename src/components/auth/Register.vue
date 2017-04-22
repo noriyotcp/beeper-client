3ced5a9db3b71a0b23955634d69811c6af155c6d
noriyotcp <noriyo.akita@gmail.com>
2017-04-22 14:10:59 +0900

    [Register] Bind data with an each field and console.log when clicking register button

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -2,13 +2,14 @@
   <div>
     <h3 class="text-center">Login</h3>
 
-    <input type="email" class="form-control m-b-15" placeholder="Email address">
-    <input type="text" class="form-control m-b-15" placeholder="Username">
-    <input type="password" class="form-control m-b-15" placeholder="Password">
+    <input type="email" class="form-control m-b-15" placeholder="Email address" v-model="user.email">
+    <input type="text" class="form-control m-b-15" placeholder="Username" v-model="user.username">
+    <input type="password" class="form-control m-b-15" placeholder="Password" v-model="user.password">
 
     <hr>
 
-    <button class="btn btn-lg btn-primary btn-block m-b-15">Register</button>
+    <button class="btn btn-lg btn-primary btn-block m-b-15"
+      @click="register">Register</button>
 
     <p class="text-center">
       Already have an account? <router-link to="/auth/login">Login!</router-link>
@@ -18,7 +19,21 @@
 
 <script>
 export default {
-  name: "register"
+  name: "register",
+  data: function() {
+    return {
+      user: {
+        email: "",
+        username: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    register: function() {
+      console.log(this.user);
+    }
+  }
 }
 </script>
 
