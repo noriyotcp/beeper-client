d10734ebbd85c82c1302d74c65f21ea3cd4b3c36
noriyotcp <noriyo.akita@gmail.com>
2017-04-22 13:48:59 +0900

    [Auth] Use transition to add fade-in/out when switching the page

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -4,7 +4,9 @@
       <div class="row">
         <div class="col-md-4 col-md-offset-4 authForm">
           <h2 class="text-center">Beeper</h2>
-          <router-view></router-view>
+          <transition name="fade">
+            <router-view></router-view>
+          </transition>
         </div>
       </div>
     </div>
