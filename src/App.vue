ac28ab9fd644b771b76df7c8708cd8f8469dada3
noriyotcp <noriyo.akita@gmail.com>
2017-04-22 13:28:33 +0900

    Move Vue's default style into beeper.css

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,4 @@
 <script>
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/beeper.css"></style>
