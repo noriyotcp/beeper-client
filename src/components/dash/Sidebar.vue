0ea6edea480318ba8bf14a05806fa28640744a7c
noriyotcp <noriyo.akita@gmail.com>
2017-04-23 21:34:17 +0900

    [Sidebar] Hide the user info when the user has not logged in
    instead, show welcome message and register/login links when the user has not logged in

--- src/components/dash/Sidebar.vue
+++ src/components/dash/Sidebar.vue
@@ -1,33 +1,43 @@
 <template lang="html">
   <div id="sidebar" class="text-center">
-    <h4 class="text-center">@{{user.username}}</h4>
-    <hr>
-    <div class="row">
+    <div v-if="this.loggedIn">
+      <h4 class="text-center">@{{user.username}}</h4>
+      <hr>
+      <div class="row">
         <div class="col-sm-6">
-            <router-link :to="'/profile/' + user.username" class="btn btn-block btn-default"><i class="fa fa-user"></i> Profile</router-link>
+          <router-link :to="'/profile/' + user.username" class="btn btn-block btn-default"><i class="fa fa-user"></i> Profile</router-link>
         </div>
         <div class="col-sm-6">
-            <router-link to="/newsfeed" class="btn btn-block btn-default"><i class="fa fa-newspaper-o"></i> Feed</router-link>
+          <router-link to="/newsfeed" class="btn btn-block btn-default"><i class="fa fa-newspaper-o"></i> Feed</router-link>
         </div>
-    </div>
+      </div>
 
-    <div class="beepNowWrap m-t-20 m-b-20">
+      <div class="beepNowWrap m-t-20 m-b-20">
         <textarea class="form-control" rows="10" placeholder="Start writing your beep here!" maxlength="320" v-model="newBeep"></textarea>
         <p class="text-muted">{{320 - newBeep.length}} character{{320 - newBeep.length == 1 ? '' : 's'}} remaining</p>
         <p class="text-center no-margin"><button class="btn btn-primary">Beep Now!</button></p>
-    </div>
+      </div>
 
-    <div class="row">
+      <div class="row">
         <div class="col-sm-6">
-            <p class="text-center no-margin">
-                <a href="#"><i class="fa fa-cog"></i> Settings</a>
-            </p>
+          <p class="text-center no-margin">
+            <a href="#"><i class="fa fa-cog"></i> Settings</a>
+          </p>
         </div>
         <div class="col-sm-6">
-            <p class="text-center no-margin">
-                <a href="#" @click.prevent="logout"><i class="fa fa-power-off"></i> Logout</a>
-            </p>
+          <p class="text-center no-margin">
+            <a href="#" @click.prevent="logout"><i class="fa fa-power-off"></i> Logout</a>
+          </p>
         </div>
+      </div>
+    </div>
+    <div v-if="!this.loggedIn">
+      <h1>Welcome!</h1>
+        <p>Beeper is a microblogging social network, come join the fun!</p>
+        <p class="text-center">
+          <router-link to="/auth/register" class="btn btn-primary btn-block">Register</router-link>
+          <router-link to="/auth/login" class="btn btn-default btn-block">Login</router-link>
+        </p>
     </div>
   </div>
 </template>
@@ -36,12 +46,15 @@
   export default {
     name: 'sidebar',
     created: function() {
-      this.getUser();
+      if (this.loggedIn) {
+        this.getUser();
+      }
     },
     data: function() {
       return {
         user: {},
-        newBeep: ""
+        newBeep: "",
+        loggedIn: this.$auth.loggedIn()
       }
     },
     methods: {
