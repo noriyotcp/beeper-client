4d53ff2b874c303c5fed73f04ecb4cf2dc7dfb9b
noriyotcp <noriyo.akita@gmail.com>
2017-04-23 17:13:37 +0900

    Show spinner when loading beeps

--- src/components/dash/BeepList.vue
+++ src/components/dash/BeepList.vue
@@ -3,6 +3,9 @@
     <div id="beepsWrapper">
       <beep v-for="beep in beeps" :beep="beep" :key="beep.id"></beep>
     </div>
+    <div id="beepsLoading" class="text-center" v-show="this.beepsLoading">
+      <i class="fa fa-spin fa-spinner"></i>
+    </div>
   </div>
 
 </template>
@@ -21,18 +24,27 @@
 
       window.addEventListener('scroll', this.handleScroll);
     },
+    destroyed: function() {
+      window.removeEventListener('scroll', this.handleScroll);
+    },
     data: function() {
       return {
         beeps: [],
-        page: {}
+        page: {},
+        beepsLoading: false
       }
     },
     methods: {
       getBeeps: function(page) {
+        this.beepsLoading = true;
         this.$http.get('/beeps?page=' + page)
             .then(function(res) {
               this.beeps = this.beeps.concat(res.body.data);
               this.page  = { current: res.body.current_page, last: res.body.last_page };
+              this.beepsLoading = false;
+            })
+            .catch(function() {
+              this.beepsLoading = false;
             })
       },
       handleScroll: function() {
